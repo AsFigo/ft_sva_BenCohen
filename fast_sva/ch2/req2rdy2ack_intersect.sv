e1ef81566fadc92cd4e6bf6a32679c0b56ede784
Ajeetha Kumari Venkatesan <ajeethak@asfigo.com>
2025-06-20 23:51:49 +0530

    Fixed formatting with Verible. Also fixed few minor typo-errors marked with AF in the code

--- fast_sva/ch2/req2rdy2ack_intersect.sv
+++ fast_sva/ch2/req2rdy2ack_intersect.sv
@@ -1,63 +1,90 @@
 module reardyack;
-  timeunit 1ns;  timeprecision 100ps;    
-  `include "uvm_macros.svh"   import uvm_pkg::*;
+  timeunit 1ns; timeprecision 100ps;
+  `include "uvm_macros.svh"
+  import uvm_pkg::*;
   bit clk, req, rdy, ack;
   initial forever #10 clk = !clk;
   initial begin
     $timeformat(-9, 0, " ns", 10);
     $display("%t", $realtime);
   end
-  
-    
-property p_req2rdy2ack;
-    @(posedge clk) $rose(req) ##[1:2] rdy |->  ##[1:2] ack;  
-endproperty
-ap_req2rdy2ack: assert property( p_req2rdy2ack);  
 
-ap_req_in2_rdy_ack: assert property(  
-    @(posedge clk) $rose(req) ##1 
-                   (rdy[->1] intersect 1[*1:2]) |->  ##[1:2] ack); 
 
-  
-  always  @(posedge clk)  begin // emulate the task to evaluate the property 
-    fork 
-        t_req_rdy_ack ();  // Evaluate the property concurrently
+  property p_req2rdy2ack;
+    @(posedge clk) $rose(
+        req
+    ) ##[1:2] rdy |-> ##[1:2] ack;
+  endproperty
+  ap_req2rdy2ack :
+  assert property (p_req2rdy2ack);
+
+  ap_req_in2_rdy_ack :
+  assert property (@(posedge clk) $rose(
+      req
+  ) ##1 
+                   (rdy [-> 1] intersect 1 [* 1: 2]) |-> ##[1:2] ack);
+
+
+  always @(posedge clk) begin  // emulate the task to evaluate the property 
+    fork
+      t_req_rdy_ack();  // Evaluate the property concurrently
     join_none
-end
-task automatic t_req_rdy_ack(); // the property 
-    bit match, pass; // results: 
+  end
+  task automatic t_req_rdy_ack();  // the property 
+    bit match, pass;  // results: 
     // The manager launching threads (other automatic tasks) and processing results 
-    fork t_req_rdy(match);  join
-    if(match) begin //  antecedent thread matches
-        fork 
-          t_ack(pass); // continue to the evaluation of the ack
-        join
-        am_reqrdyack:  assert(pass) $display("%t req rdy ack assertion is PASS", $realtime); 
-                      else $display("%t req rdy ack assertion is FAIL", $realtime); 
-    end
-    else  $display("%t req rdy ack assertion is vacuous", $realtime);  
-endtask 
+    fork
+      t_req_rdy(match);
+    join
+    if (match) begin  //  antecedent thread matches
+      fork
+        t_ack(pass);  // continue to the evaluation of the ack
+      join
+      am_reqrdyack :
+      assert (pass) $display("%t req rdy ack assertion is PASS", $realtime);
+      else $display("%t req rdy ack assertion is FAIL", $realtime);
+    end else $display("%t req rdy ack assertion is vacuous", $realtime);
+  endtask
 
-task automatic  t_req_rdy (inout bit match);
+  task automatic t_req_rdy(inout bit match);
     // ($rose(req) ##1 rdy) 
-    if($rose(req))  @(posedge clk) if(rdy) match=1;  else match=0;
-endtask 
+    if ($rose(req))
+      @(posedge clk)
+        if (rdy) match = 1;
+        else match = 0;
+  endtask
 
-task automatic t_ack (inout bit pass); // ack after 1 cycle
-   @(posedge clk) if(ack) pass=1; else pass=0;    
-endtask  
+  task automatic t_ack(inout bit pass);  // ack after 1 cycle
+    @(posedge clk)
+      if (ack) pass = 1;
+      else pass = 0;
+  endtask
 
   initial begin
     //$dumpfile("dump.vcd"); $dumpvars;
     bit v_a, v_b, v_c;
     repeat (1000) begin
       @(posedge clk);
-      if (!randomize(v_a, v_b, v_c) with {
-        v_a   dist {1'b1 := 1, 1'b0 := 1};
-        v_b   dist {1'b1 := 1, 1'b0 := 1};
-        v_c   dist {1'b1 := 1, 1'b0 := 1};
-      }) `uvm_error("MYERR", "This is a randomize error");
-      req <= v_a; rdy<=v_b; ack<=v_c; 
+      if (!randomize(
+              v_a, v_b, v_c
+          ) with {
+            v_a dist {
+              1'b1 := 1,
+              1'b0 := 1
+            };
+            v_b dist {
+              1'b1 := 1,
+              1'b0 := 1
+            };
+            v_c dist {
+              1'b1 := 1,
+              1'b0 := 1
+            };
+          })
+        `uvm_error("MYERR", "This is a randomize error");
+      req <= v_a;
+      rdy <= v_b;
+      ack <= v_c;
     end
     $finish;
   end
--- fast_sva/ch2/reqack_repeats.sv
+++ fast_sva/ch2/reqack_repeats.sv
@@ -1,29 +1,41 @@
 module reqack_repeats;
-  timeunit 1ns;  timeprecision 100ps;    
-  `include "uvm_macros.svh"   import uvm_pkg::*;
+  timeunit 1ns; timeprecision 100ps;
+  `include "uvm_macros.svh"
+  import uvm_pkg::*;
   bit clk, req, ack;
-  bit[0:1] tv[0:15] =
-    // req ack
-     {0, 2, 2, 3, 3, 1, 0, 0,
-      2, 2, 2, 3, 3, 0, 0, 0 }; 
+  bit [0:1] tv[0:15] =
+  // req ack
+  {
+    0, 2, 2, 3, 3, 1, 0, 0, 2, 2, 2, 3, 3, 0, 0, 0
+  };
   initial forever #10 clk = !clk;
   initial begin
     $timeformat(-9, 0, " ns", 10);
     $display("%t", $realtime);
   end
 
-  ap_req_ack: assert property(@ (posedge clk)  $rose(req) |->  // in the same cycle 
-         req && !ack[*3]  // req==1 and ack==0 for 3 cycles, including the current cycle     
-    ##1  req &&  ack[*2]  // at the next cycle, req==1 for 2 cycles, don’t care about req
-    ##1 !req && !ack);    // at the next cycle, req==0 and ack==0 for 1 cycles
- 
+  ap_req_ack :
+  assert property (@(posedge clk) $rose(
+      req
+  ) |->  // in the same cycle 
+  req && !ack [* 3]  // req==1 and ack==0 for 3 cycles, including the current cycle     
+  ##1 req && ack [* 2]  // at the next cycle, req==1 for 2 cycles, don’t care about req
+  ##1 !req && !ack);  // at the next cycle, req==0 and ack==0 for 1 cycles
 
-    ap_req_ack_range: assert property(@ (posedge clk)  $rose(req) |->  // in the same cycle 
-    req[*1:3] // req==1 for 1 to 3 cycles  
-    ##1 $rose(ack)  // until it is followed by a rise of ack, ack was 0    
-    ##1 ack[*1:2]   // ack holds for a max of 2 cycles 
-    ##0 $fell(req)  // until req==0    
-    ##1 !req && !ack);    // at the next cycle, req==0 and ack==0 for 1 cycles
+
+  ap_req_ack_range :
+  assert property (@(posedge clk) $rose(
+      req
+  ) |->  // in the same cycle 
+  req [* 1: 3]  // req==1 for 1 to 3 cycles  
+  ##1 $rose(
+      ack
+  )  // until it is followed by a rise of ack, ack was 0    
+  ##1 ack [* 1: 2]  // ack holds for a max of 2 cycles 
+  ##0 $fell(
+      req
+  )  // until req==0    
+  ##1 !req && !ack);  // at the next cycle, req==0 and ack==0 for 1 cycles
 
 
 
@@ -32,19 +44,32 @@ module reqack_repeats;
   initial begin
     //$dumpfile("dump.vcd"); $dumpvars;
     bit v_a, v_b, v_c;
-     
-    for (int i = 0; i<=15 ;i++ ) begin
-         @(posedge clk);
-        {req, ack} <= tv[i];  
+
+    for (int i = 0; i <= 15; i++) begin
+      @(posedge clk);
+      {req, ack} <= tv[i];
     end
     repeat (1000) begin
       @(posedge clk);
-      if (!randomize(v_a, v_b, v_c) with {
-        v_a   dist {1'b1 := 3, 1'b0 := 1};
-        v_b   dist {1'b1 := 2, 1'b0 := 1};
-        v_c   dist {1'b1 := 2, 1'b0 := 1};
-      }) `uvm_error("MYERR", "This is a randomize error");
-      req <= v_a;  ack<=v_c; 
+      if (!randomize(
+              v_a, v_b, v_c
+          ) with {
+            v_a dist {
+              1'b1 := 3,
+              1'b0 := 1
+            };
+            v_b dist {
+              1'b1 := 2,
+              1'b0 := 1
+            };
+            v_c dist {
+              1'b1 := 2,
+              1'b0 := 1
+            };
+          })
+        `uvm_error("MYERR", "This is a randomize error");
+      req <= v_a;
+      ack <= v_c;
     end
     $finish;
   end
